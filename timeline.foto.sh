b5561c203c9b29f3b91fa61736eee27a1c95845a
marcostrfn <marcos.trfn@gmail.com>
2017-03-23 18:05:43 +0100

    Update timeline.foto.sh

--- timeline.foto.sh
+++ timeline.foto.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# fswebcam -r 1280x720 --no-banner /home/pi/webcam/$DATE.jpg
 
 DATE=$(date +"%Y-%m-%d_%H%M%S")
+# fswebcam -r 1280x720 --no-banner /home/pi/webcam/$DATE.jpg
 fswebcam /var/www/html/3d/$DATE.jpg
-
